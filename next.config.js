a0864f7961029bea57ca09f1a92e3b5c5b7cda97
Just Cosmetics <justcosmeticscy@gmail.com>
2025-10-29 18:12:34 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,6 +18,18 @@ const nextConfig = {
         hostname: 'via.placeholder.com',
         pathname: '**',
       },
+      // ADD YOUR WP staging host:
+      {
+        protocol: 'https',
+        hostname: 'staging-f302-justcosmeticscy.wpcomstaging.com',
+        pathname: '**',
+      },
+      // (Optional) when you switch to live WP media later, add:
+      // {
+      //   protocol: 'https',
+      //   hostname: 'justcosmetics.cy',
+      //   pathname: '**',
+      // },
     ],
   },
 };
