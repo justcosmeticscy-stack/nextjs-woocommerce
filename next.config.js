d1dc9a9f7507c69b39729e2c87ca63a133284013
Just Cosmetics <justcosmeticscy@gmail.com>
2025-10-29 19:02:29 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,33 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'swewoocommerce.dfweb.no',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        pathname: '**',
-      },
-      // ADD YOUR WP staging host:
-      {
-        protocol: 'https',
-        hostname: 'staging-f302-justcosmeticscy.wpcomstaging.com',
-        pathname: '**',
-      },
-      // (Optional) when you switch to live WP media later, add:
-      // {
-      //   protocol: 'https',
-      //   hostname: 'justcosmetics.cy',
-      //   pathname: '**',
-      // },
+      { protocol: 'https', hostname: 'swewoocommerce.dfweb.no', pathname: '**' },
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '**' },
+      { protocol: 'https', hostname: 'via.placeholder.com', pathname: '**' },
+      // ADD YOUR WP staging host
+      { protocol: 'https', hostname: 'staging-f302-justcosmeticscy.wpcomstaging.com', pathname: '**' },
     ],
   },
 };
